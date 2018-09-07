38c86aebc0a7ba084411d9474b28bcef458718f0
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-07 19:07:11 +0200

    add lambda

--- src/main/java/anonymousClass/Main.java
+++ src/main/java/anonymousClass/Main.java
@@ -1,33 +1,56 @@
 package anonymousClass;
 
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
 public class Main {
 
 
-    public interface Button {
+    public interface ButtonT {
         void click();
 
-        void mouseOn();
+//        void mouseOn();
     }
 
     public static void action() {
-        Button button = new Button() {
+        ButtonT button = new ButtonT() {
             @Override
             public void click() {
                 System.out.println("Przycisk klikniety");
             }
 
-            @Override
-            public void mouseOn() {
-                System.out.println("Mysz poruszona");
-            }
+//            @Override
+//            public void mouseOn() {
+//                System.out.println("Mysz poruszona");
+//            }
         };
 
         button.click();
-        button.mouseOn();
+//        button.mouseOn();
     }
 
     public static void main(String[] args) {
-action();
+        ButtonT button = () ->
+                System.out.println("Przycisk klikniety");
+        button.click();
+
+        Runnable t1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Jestem w nowym watku");
+            }
+        };
+
+        Runnable r1 = () -> System.out.println("Jestem w kolejnym watku");
+        r1.run();
+
+        Button btn = new Button();
+
+        btn.setText("Test");
+        btn.setOnAction(event -> System.out.println("Przycisk zostal klikniety"));
+
 
     }
 }
