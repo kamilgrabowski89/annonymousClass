345bc8f9530db1a6469d21a285111eca36ea91a1
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-07 18:26:12 +0200

    add new method

--- src/main/java/anonymousClass/Main.java
+++ src/main/java/anonymousClass/Main.java
@@ -2,7 +2,32 @@ package anonymousClass;
 
 public class Main {
 
+
+    public interface Button {
+        void click();
+
+        void mouseOn();
+    }
+
+    public static void action() {
+        Button button = new Button() {
+            @Override
+            public void click() {
+                System.out.println("Przycisk klikniety");
+            }
+
+            @Override
+            public void mouseOn() {
+                System.out.println("Mysz poruszona");
+            }
+        };
+
+        button.click();
+        button.mouseOn();
+    }
+
     public static void main(String[] args) {
+action();
 
     }
 }
